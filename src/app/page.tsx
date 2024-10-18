7a80ecf54403da1c226724e3ec420f02627368e3
ImMiguelFv <mferronvinuela@gmail.com>
2024-10-18 10:36:19 +0200

    mmm

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <form action="">
+      <input type="text" placeholder="Email Title" />
+      <input type="email" placeholder="To" />
+      <textarea name="content"></textarea>
+      <button>Send</button>
+      </form>
+      </div>
+  )
+}
