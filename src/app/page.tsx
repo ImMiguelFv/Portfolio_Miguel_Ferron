d4410ab2d97f0a209b5e8804d7dd295acc49208c
ImMiguelFv <mferronvinuela@gmail.com>
2024-10-18 11:04:42 +0200

    mm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 export default function Home() {
   return (
-    <div>
-      <form action="">
-      <input type="text" placeholder="Email Title" />
-      <input type="email" placeholder="To" />
-      <textarea name="content"></textarea>
-      <button>Send</button>
+    <div className="flex items-center justify-center h-screen">
+      <form action="" className="flex flex-col gap-y-4">
+      <input type="text" placeholder="Email Title" 
+      className="bg-slate-400 block p-2 rounded-md placeholder:text-slate-500"/>
+      <input type="email" placeholder="To"
+      className="bg-slate-400 block p-2 rounded-md placeholder:text-slate-500"/>
+      <textarea name="content" placeholder="Write the mail"
+      className="bg-slate-400 block p-2 rounded-md placeholder:text-slate-500">
+      </textarea>
+      <button className="bg-green-600 block p-2 rounded-md hover:bg-green-400">Send</button>
       </form>
       </div>
   )
