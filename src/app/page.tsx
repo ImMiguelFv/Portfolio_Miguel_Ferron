5442f3df069fb1c83b8f59f62a8dd2141e5a89a4
ImMiguelFv <mferronvinuela@gmail.com>
2024-10-18 11:14:14 +0200

    inpu mail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 export default function Home() {
+
+  async function sendMail(formData: FormData) {
+  "use server"
+  const title = formData.get("title")
+  const to = formData.get("to")
+  const content = formData.get("content")
+
+  if (!title || !to || !content) {
+    throw new Error("Missing fields")
+  }
+  console.log({title, to, content})
+}
   return (
     <div className="flex items-center justify-center h-screen">
-      <form action="" className="flex flex-col gap-y-4">
-      <input type="text" placeholder="Email Title" 
+      <form action={sendMail} className="flex flex-col gap-y-4">
+      <input name="title" type="text" placeholder="Email Title" 
       className="bg-slate-400 block p-2 rounded-md placeholder:text-slate-500"/>
-      <input type="email" placeholder="To"
+      <input name="to" type="email" placeholder="To"
       className="bg-slate-400 block p-2 rounded-md placeholder:text-slate-500"/>
       <textarea name="content" placeholder="Write the mail"
       className="bg-slate-400 block p-2 rounded-md placeholder:text-slate-500">
